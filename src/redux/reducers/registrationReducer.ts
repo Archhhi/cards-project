dcb9fdc4a059fd65fcacb56dd8981011f20bc8a3
Archhhi <brabus-20@mail.ru>
2021-06-27 18:20:08 +0300

    updated

--- src/redux/reducers/registrationReducer.ts
+++ src/redux/reducers/registrationReducer.ts
@@ -1,3 +1,32 @@
-export const registrationReducer = (state = {}, action: any) => {
-  return state
+import {Dispatch} from "redux"
+import {authAPI} from "../../api/api"
+
+const initialState = {
+    isRegister: false
+}
+type InitialStateType = typeof initialState
+
+export const registrationReducer = (state: InitialStateType = initialState, action: ActionsType) => {
+    switch (action.type) {
+        case 'login/SET-IS-REGISTER':
+            debugger
+            return {...state, isRegister: action.value}
+        default:
+            return state
+    }
+}
+
+export const setIsRegisterAC = (value: boolean) =>
+    ({type: 'login/SET-IS-REGISTER', value} as const)
+type ActionsType = ReturnType<typeof setIsRegisterAC>
+
+export const registerTC = (email: string, password: string) => {
+    return async (dispatch: Dispatch<ActionsType>) => {
+        try {
+            const response = await authAPI.auth(email, password)
+            dispatch(setIsRegisterAC(true))
+        } catch (e) {
+            alert(e)
+        }
+    }
 }
