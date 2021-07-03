cd0e1d444856c6614f7a073488fd9e5efe181ef3
Archhhi <brabus-20@mail.ru>
2021-07-03 12:09:28 +0300

    bug fix with auth redirect

--- src/component/Profile/ProfilePage.tsx
+++ src/component/Profile/ProfilePage.tsx
@@ -2,14 +2,13 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getAuthUserData} from "../../redux/reducers/loginReducer";
 import {RootStateType} from "../../redux/store";
+import {Redirect} from "react-router-dom";
 
 const ProfilePage = () => {
   const isAuth = useSelector<RootStateType, boolean>(state => state.login.isAuth)
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getAuthUserData())
-  },[isAuth])
+
+  if(!isAuth) return <Redirect to={'/profile'}/>
 
   return (
     <div>
