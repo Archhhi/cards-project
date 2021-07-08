cd11ad48b7df56c0ec307a90b2bd18bf002bef34
Archhhi <brabus-20@mail.ru>
2021-07-08 19:05:15 +0300

    fix bug and merge

--- src/components/PacksList/PacksList.tsx
+++ src/components/PacksList/PacksList.tsx
@@ -51,30 +51,26 @@ const PacksList: React.FC = React.memo(() => {
     minCardsCount
   } = useSelector<RootStateType, PacksStateType>(state => state.packs)
   const {_id, isAuth} = useSelector<RootStateType, AuthStateType>(state => state.login)
+  const [rangeValues, setRangeValues] = useState([settedMinCardsValue, settedMaxCardsValue])
 
   useEffect(() => {
-    cardPacks.length === 0 && dispatch(getPacksTC())
+    // cardPacks.length === 0 && dispatch(getPacksTC())
+    cardPacks.length === 0 && dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
   }, [cardPacks])
 
   useEffect(() => {
     if (onMode === 'all') {
-      dispatch(getPacksTC())
+      // dispatch(getPacksTC())
+      dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
     } else if (onMode === 'my') {
-      dispatch(getPacksTC(_id))
+      // dispatch(getPacksTC(_id))
+      dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1], _id))
     }
   }, [searchInputValue])
 
   if (onMode === 'pending') {
     dispatch(setOnMode('all'))
   }
-
-  // useEffect(() => {
-  //     dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
-  // }, [])
-  // console.log("page= ", page,
-  //     "pageCount= ", pageCount,
-  //     "cardPacksTotalCount =", cardPacksTotalCount)
-  //
 
   if (!isAuth) return <Redirect to={'/login'}/>
 
@@ -103,42 +99,37 @@ const PacksList: React.FC = React.memo(() => {
   }
 
   const myStyle = {
-    backgroundColor: onMode ? '#9A91C8' : '#FFFFFF'
+    backgroundColor: onMode === 'my' ? '#9A91C8' : '#FFFFFF'
   }
   const allStyle = {
-    backgroundColor: onMode ? '#FFFFFF' : '#9A91C8'
+    backgroundColor: onMode === 'all' || onMode === 'pending' ? '#9A91C8' : '#FFFFFF'
   }
 
   const onMyPacks = () => {
     dispatch(setOnMode('my'))
-    dispatch(getPacksTC(_id))
+    // dispatch(getPacksTC(_id))
+    dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1], _id))
   }
   const onAllPacks = () => {
     dispatch(setOnMode('all'))
-    dispatch(getPacksTC())
+    // dispatch(getPacksTC())
+    dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
   }
-  // const onMyPacks = () => {
-  //     setOnMode(true)
-  //     dispatch(getPacksTC(_id))
-  //     dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1], _id))
-  // }
-  // const onAllPacks = () => {
-  //     setOnMode(false)
-  //     dispatch(getPacksTC())
-  //     dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
-  // }
-  //
-  // const onInputValuesEntered = () => {
-  //     if (onMode){
-  //     dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1], _id))
-  //     } else {
-  //         dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
-  //     }
-  // }
-  //
-  // const paginate = (page: number) => {
-  //     dispatch(getPacksByPageNumberTC(page, settedMinCardsValue, settedMaxCardsValue,))
-  // }
+
+  const onInputValuesEntered = () => {
+    if (onMode === 'my') {
+      dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1], _id))
+    } else if (onMode === 'all') {
+      dispatch(getSortedPacksTC(rangeValues[0], rangeValues[1]))
+    }
+  }
+
+  const minValueForRangeComponent = minCardsCount ? minCardsCount : undefined
+  const maxValueForRangeComponent = maxCardsCount ? maxCardsCount : undefined
+
+  const paginate = (page: number) => {
+    dispatch(getPacksByPageNumberTC(page, settedMinCardsValue, settedMaxCardsValue,))
+  }
 
   function onChange(value: string) {
     dispatch(setSearchInputValue(value))
