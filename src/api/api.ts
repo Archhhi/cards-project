9310384cff6afd7defdce17fb836777a77b541ed
Archhhi <brabus-20@mail.ru>
2021-07-09 18:42:21 +0300

    fix api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,7 +9,7 @@ export type AuthResponseType = {
 }
 
 const instance = axios.create({
-  baseURL:"http://localhost:7542/2.0/",
+  baseURL:"https://neko-back.herokuapp.com/2.0",
   withCredentials: true,
 })
 
